b7897b7344e53ed46b052257c1de2e7d56896470
吴云 (Daniel) <Yun.Wu1@geely.com>
2018-07-05 17:09:55 +0800

    logger

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cep_report_auto
-cep_report_auto
+
+## CEP报表相关页面的自动化工程
--- common/logger.py
+++ common/logger.py
@@ -19,6 +19,30 @@ class Logger(object):
         self.logger = logging.getLogger(logger)
         self.logger.setLevel(logging.DEBUG)
         ct = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-        log_path = os.path.dirname(os.getcwd() + '/logs')
+        log_path = os.path.dirname(os.path.abspath('.')) + '/logs/'
+        if not os.path.exists(log_path):
+            os.mkdir(log_path)
+        else:
+            pass
         log_file_name = log_path + ct + '.log'
         fh = logging.FileHandler(log_file_name)
+        fh.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.WARNING)
+        formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
+
+    def ge_log(self):
+        return self.logger
+
+
+if __name__ == '__main__':
+    log = Logger('test').ge_log()
+    log.info('info')
+    log.debug('debug')
+    log.warning('warning')
+    log.error('error')
